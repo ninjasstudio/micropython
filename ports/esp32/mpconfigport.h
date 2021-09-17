e55e16c07cf2d2b4e394ebb2f328c67e7b000b31
IhorNehrutsa <Ihor.Nehrutsa@gmail.com>
2021-09-17 10:32:52 +0300

    SOFTI2C SOFTSPI ONEWIRE

--- ports/esp32/mpconfigport.h
+++ ports/esp32/mpconfigport.h
@@ -217,7 +217,9 @@ extern const struct _mp_obj_module_t uos_module;
 extern const struct _mp_obj_module_t mp_module_usocket;
 extern const struct _mp_obj_module_t mp_module_machine;
 extern const struct _mp_obj_module_t mp_module_network;
+#if MICROPY_PY_ONEWIRE
 extern const struct _mp_obj_module_t mp_module_onewire;
+#endif
 
 #define MICROPY_PORT_BUILTIN_MODULES \
     { MP_OBJ_NEW_QSTR(MP_QSTR_esp), (mp_obj_t)&esp_module }, \
