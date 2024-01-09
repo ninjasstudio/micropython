5fbf4f80822ec9a608fd75cd0f213a46db095dbe
IhorNehrutsa <IhorNehrutsa@gmail.com>
2024-01-09 14:58:28 +0200

    esp32: Rename GENERIC_C6 to ESP32_GENERIC_C6.
    Signed-off-by: IhorNehrutsa <Ihor.Nehrutsa@gmail.com>

--- ports/esp32/boards/ESP32_GENERIC_C6/mpconfigboard.cmake
+++ ports/esp32/boards/ESP32_GENERIC_C6/mpconfigboard.cmake
@@ -3,7 +3,8 @@ set(IDF_TARGET esp32c6)
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     #boards/sdkconfig.ble
-    boards/GENERIC_C6/sdkconfig.board
+    boards/ESP32_GENERIC_C6/sdkconfig.board
 )
 
 #set(CONFIG_BT_NIMBLE_LEGACY_VHCI_ENABLE "y")
+set(CONFIG_ESP_CONSOLE_UART_BAUDRATE 115200)
